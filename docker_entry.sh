c64cf814ab6d5a3920a75e3abd382bb25df23bbe
Mathew Moon <mmoon@athena-iot.com>
2019-02-07 17:51:11 -0600

    Allow specifying either both or a single service via environment variables

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 export PHP_FPM_USER="www" PHP_FPM_GROUP="www" PHP_DISPLAY_ERRORS="On"
-php-fpm7 -O -D -c /etc/php7/php.ini
-nginx -g "daemon off;"
+
+if [ -z ${SERVICE} ]; then
+  php-fpm && php-fpm7 -O -D -c /etc/php7/php.ini
+  nginx && nginx -g "daemon off;"
+else
+ [ ${SERVICE} = nginx ] && nginx && nginx -g "daemon off;"
+ [ ${SERVICE} = php-fpm ] && php-fpm && php-fpm7 -O -F -c /etc/php7/php.ini
+fi
