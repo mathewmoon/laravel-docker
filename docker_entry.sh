d626ccf166420262bfcf8157a4a0b7196047b392
Mathew Moon <mmoon@athena-iot.com>
2019-02-07 17:58:35 -0600

    Add 'bind to all addresses' option for php-fpm

--- docker_entry.sh
+++ docker_entry.sh
@@ -2,6 +2,13 @@
 
 export PHP_FPM_USER="www" PHP_FPM_GROUP="www" PHP_DISPLAY_ERRORS="On"
 
+[ -z ${PHP_PORT} ] && PHP_PORT=9000
+
+#If you are running php by itself then you probably want to bind to all addresses so your frontend can connect
+[ ! -z ${PHP_BIND_ALL} ]; then
+  sed -ri "s/^listen\s=\s.*/listen = 0.0.0.0:${PHP_PORT}/g" /etc/php7/php-fpm.d/www.conf
+fi
+
 if [ -z ${SERVICE} ]; then
   php-fpm && php-fpm7 -O -D -c /etc/php7/php.ini
   nginx && nginx -g "daemon off;"
